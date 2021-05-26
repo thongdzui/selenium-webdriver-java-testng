3d3fe0798b107464f84f63ded700dcd01190010e
Thong Truong <thong3108@gmail.com>
2021-05-26 23:21:02 +0700

    Complete 6 bài Exercise Topic 6, video Topic 13

--- selenium/Webdriver/Topic_05_Web_Element_Exercise.java
+++ selenium/Webdriver/Topic_05_Web_Element_Exercise.java
@@ -15,6 +15,10 @@ public class Topic_05_Web_Element_Exercise {
 	//khai báo biến (declare)
 	WebDriver driver;
 	String firstName, lastName, fullName, emailAddress, password;
+	
+	//By passwordTextbox = By.id("password");
+	//By disableCheckbox = By.id("check-disabled");
+	//By disableButton = By.id("button-disabled");
 
 	
 	@BeforeClass
@@ -34,7 +38,7 @@ public class Topic_05_Web_Element_Exercise {
 		
 	}
 
-	@Test
+	//@Test
 	public void TC_01_Create_New_Account() {
 		driver.get("http://live.demoguru99.com/");
 		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
@@ -66,7 +70,7 @@ public class Topic_05_Web_Element_Exercise {
 		
 	}
 
-	@Test
+	//@Test
 	public void TC_02_Login() {
 		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
 		driver.findElement(By.cssSelector("#email")).sendKeys(emailAddress);
@@ -78,13 +82,219 @@ public class Topic_05_Web_Element_Exercise {
 		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText(), "Hello, " + fullName + "!");
 	}
 
-	@Test
+	//@Test
 	public void TC_03_() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		//hàm kiểm tra đk
+		//nếu đk đúng thì mới vào hàm if
+		//sai thì ko vào
+		if(driver.findElement(By.id("mail")).isDisplayed()) {
+			driver.findElement(By.id("mail")).sendKeys("Automation FC");
+			System.out.println("Mail text box is displayed");
+		} else {
+			System.out.println("Mail text box is not displayed");
+		}
+		
+		if(driver.findElement(By.id("edu")).isDisplayed()) {
+			driver.findElement(By.id("edu")).sendKeys("Automation FC");
+			System.out.println("Education text area is displayed");
+		} else {
+			System.out.println("Education text area is not displayed");
+		}
+		
+		if(driver.findElement(By.id("under_18")).isDisplayed()) {
+			driver.findElement(By.id("under_18")).sendKeys("Automation FC");
+			System.out.println("Radio button 'under 18' is clicked");
+		} else {
+			System.out.println("Radio button 'under 18' is not clicked");
+		}
 		
 		
 	}
+	//@Test
+	public void TC_03_Function() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		By emailTextBox = By.id("mail");
+		By educationTextArea = By.id("edu");
+		By under18radio = By.id("under_18");
+		
+		if(isElementDisplayed(emailTextBox)) {
+			sentkeyToElement(emailTextBox, "Automation FC");
+		}
+		
+		if(isElementDisplayed(educationTextArea)) {
+			sentkeyToElement(educationTextArea, "Automation FC");
+		}
+		
+		if(isElementDisplayed(under18radio)) {
+			clickToElement(under18radio);
+		}
+		
+		
+	}
+	@Test
+	public void TC_04_Selected_Function() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		By under18radio = By.id("under_18");
+		By javaCheckbox = By.id("java");
+		clickToElement(under18radio);
+		clickToElement(javaCheckbox);
+		
+		//verify checkbox/ radio are selected
+		Assert.assertTrue(isElementSelected(under18radio));
+		Assert.assertTrue(isElementSelected(javaCheckbox));
+		
+		
+		clickToElement(javaCheckbox);
+		//verify checkbox is de-selected
+		Assert.assertFalse(isElementSelected(javaCheckbox));
+		Assert.assertTrue(isElementSelected(under18radio));
+		
+		
+		
+		
+	}
+	public void TC_05_Enabled() {
+		By emailTextBox = By.id("mail");
+		By educationTextArea = By.id("edu");
+		By under18radio = By.id("under_18");
+		By javaCheckbox = By.id("java");
+		By passwordTextbox = By.id("password");
+		By disableCheckbox = By.id("check-disbaled");
+		By disableButton = By.id("button-disabled");
+		
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		//Expected nó enable 
+		Assert.assertTrue(isElementEnabled(emailTextBox));
+		Assert.assertTrue(isElementEnabled(educationTextArea));
+		Assert.assertTrue(isElementEnabled(under18radio));
+		Assert.assertTrue(isElementEnabled(javaCheckbox));
+		
+		//Expecy nó disable (read only)
+		Assert.assertFalse(isElementEnabled(passwordTextbox));
+		Assert.assertFalse(isElementEnabled(disableCheckbox));
+		
+		//Mong muốn 1 element là disable
+		Assert.assertFalse(isElementEnabled(disableButton));
+		Assert.assertTrue(!isElementEnabled(disableButton));
+		
+	}
+	
+	@Test
+	public void TC_06_Register_Validate() {
+		driver.get("https://login.mailchimp.com/signup/");
+		By passwordTextbox = By.id("new_password");
+		By newletterCheckbox = By.cssSelector("#marketing_newsletter");
+		By signUpButton = By.cssSelector("#create-account");
+		By upperCaseCompleted = By.cssSelector(".uppercase-char.completed");
+		By lowerCaseCompleted = By.cssSelector(".lowercase-char.completed");
+		By numberCompleted = By.cssSelector(".number-char.completed");
+		By specialCharCompleted = By.cssSelector(".special-char.completed");
+		By greaterThan8CharCompleted = By.cssSelector("li[class='8-char completed']");
+		
+		driver.findElement(By.id("email")).sendKeys("automation@hotmail.com");
+		driver.findElement(By.id("new_username")).sendKeys("automation.net");
+		
+		//Verify Uppercase
+		driver.findElement(passwordTextbox).sendKeys("A");
+		sleepInSecond(2);
+		Assert.assertTrue(driver.findElement(upperCaseCompleted).isDisplayed());
+		Assert.assertFalse(driver.findElement(signUpButton).isEnabled());
+		
+		
+		//Verify Lowercase
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys("automation");
+		sleepInSecond(2);
+		Assert.assertTrue(isElementDisplayed(lowerCaseCompleted));
+		Assert.assertFalse(isElementEnabled(signUpButton));
+		
+		//Verify Number
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys("123456");
+		sleepInSecond(2);
+		Assert.assertTrue(isElementDisplayed(numberCompleted));
+		Assert.assertFalse(isElementEnabled(signUpButton));
+		
+		//Verify Special Char
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys("@#$@#");
+		sleepInSecond(2);
+		Assert.assertTrue(isElementDisplayed(specialCharCompleted));
+		Assert.assertFalse(isElementEnabled(signUpButton));
+		
+		//Verify Special Char
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys("dasjdhbasj");
+		sleepInSecond(2);
+		Assert.assertTrue(isElementDisplayed(greaterThan8CharCompleted));
+		Assert.assertFalse(isElementEnabled(signUpButton));
+		
+		//All criteria
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys("Automation1@");
+		
+		Assert.assertFalse(isElementDisplayed(upperCaseCompleted));
+		Assert.assertFalse(isElementDisplayed(lowerCaseCompleted));
+		Assert.assertFalse(isElementDisplayed(specialCharCompleted));
+		Assert.assertFalse(isElementDisplayed(specialCharCompleted));
+		Assert.assertFalse(isElementDisplayed(greaterThan8CharCompleted));
+		
+		Assert.assertTrue(isElementEnabled(signUpButton));
+		
+		driver.findElement(By.cssSelector("#marketing_newsletter")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("#marketing_newsletter")).isSelected());
+		
+		Assert.assertTrue(isElementSelected(newletterCheckbox));
+		
+	}
+	
+	
+	public boolean isElementDisplayed(By by) {
+		if(driver.findElement(by).isDisplayed()) {
+			System.out.println(by + " is displayed ");
+			return true;
+		} else {
+			System.out.println(by + " is not displayed ");
+			return false;
+		}
+		
+		
+	}
+	
+	public boolean isElementSelected(By by) {
+		if(driver.findElement(by).isSelected()) {
+			System.out.println(by + " is selected ");
+			return true;
+		} else {
+			System.out.println(by + " is not selected ");
+			return false;
+		}
+		
+		
+	}
+	public boolean isElementEnabled(By by) {
+		if(driver.findElement(by).isEnabled()) {
+			System.out.println(by + " is enabled ");
+			return true;
+		} else {
+			System.out.println(by + " is disabled ");
+			return false;
+		}
+		
+		
+	}
+	public void sentkeyToElement(By by, String value) {
+		driver.findElement(by).clear();
+		driver.findElement(by).sendKeys(value);
+		
+	}
+	public void clickToElement(By by) {
+		driver.findElement(by).click();
+	}
 
-	@AfterClass
+	//@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
